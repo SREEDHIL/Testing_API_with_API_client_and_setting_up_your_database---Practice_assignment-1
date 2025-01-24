11b360b72505e79fa745eeb53845c29429e73f2b
SREEDHIL PAVISHANKER B <sreedhilpavishanker@gmail.com>
2025-01-24 11:41:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,19 +33,30 @@
 
 
 const express = require('express');
-const { resolve } = require('path');
-
+const fs = require('fs');
 const app = express();
-const port = 3010;
+app.use(express.json());
+const PORT = 7010;
+const studentData = JSON.parse(fs.readFileSync('./data.json'));
 
-app.use(express.static('static'));
+app.post('/students/above-threshold', (req, res) => {
+  const { threshold } = req.body;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (threshold === undefined || typeof threshold !== 'number') {
+    res.send({ error: "'invalid input" });
+  }
+
+  const filteredStudents = studentData.filter((student) => student.total > threshold);
+
+
+  res.json({
+    count: filteredStudents.length,
+    students: filteredStudents,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
